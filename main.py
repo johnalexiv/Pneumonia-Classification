8e9461f8f3fcd43af7036f873c511bdcc3fd7381
johnalexiv <johnalexiv@u-10-81-195-178.dhcp.unlv.edu>
2018-04-26 15:56:15 -0700

    added neural network and image downsampling

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import tensorflow as tf
 import os
 import numpy as np
-
-import matplotlib.pyplot as plt
-import matplotlib.image as mpimg
+from scipy.misc import imresize
+import pylab as pl
+from PIL import Image
 
 data_dir = os.getcwd() + '/chest_xray'
 
@@ -15,6 +15,25 @@ test_pneumonia_files = os.listdir(data_dir + '/test/PNEUMONIA')
 
 val_normal_files = os.listdir(data_dir + '/val/NORMAL')
 val_pneumonia_files = os.listdir(data_dir + '/val/PNEUMONIA')
+
+path = r'\path\to\image\file.jpg'
+
+img = Image.open(os.path.join(data_dir, 'train/NORMAL', train_normal_files[20]))
+
+diff = abs(img.size[0] - img.size[1])
+padding = int(diff / 2)
+if img.size[0] > img.size[1]:
+    new_img = np.pad(img, [(padding, padding + 1), (0, 0)], mode='constant')
+else:
+    new_img = np.pad(img, [(0, 0), (padding, padding + 1)], mode='constant')
+
+new_img = imresize(new_img, (200,200), interp='bilinear')
+
+a = np.asarray(new_img)
+
+pl.imshow(a)
+pl.show()
+
 
 LOGDIR = os.path.dirname(__file__)
 SAVEPATH = r'model_saves/'
@@ -28,6 +47,9 @@ if not os.path.exists(SAVEDIR):
 if not os.path.exists(SUMDIR):
     os.mkdir(SUMDIR)
 
+def load_image():
+    pass
+
 def weight_variable(shape):
     weight = tf.truncated_normal(shape, stddev=0.1)
     return tf.Variable(weight)
@@ -36,6 +58,8 @@ def bias_variable(shape):
     bias = tf.constant(0.1, shape=shape)
     return tf.Variable(bias)
 
+
+
 def save_model(save_step, sess, saver):
     save_path = (SAVEDIR)
     if not os.path.exists(save_path):
@@ -43,21 +67,71 @@ def save_model(save_step, sess, saver):
     saver.save(sess, (save_path + '/model.ckpt'), global_step=save_step)
     print('Model saved to %s' % save_path)
 
-def neural_network(input, num_input, num_layers, num_neurons, num_classes):
-    pass
+def neural_network(input, num_input, num_layers, num_neurons, num_classes, activation_function):
+    input_channel = num_input
+    output_channel = num_neurons
+    prev_layer = input
 
-def cnn(input, num_input, num_layers, num_neurons, num_classes):
-    pass
+    if num_layers > 1:
+        for _ in range(num_layers):
+            if activation_function == 'relu':
+                out_layer = tf.nn.relu(tf.add(tf.matmul(
+                    prev_layer, weight_variable([input_channel, output_channel])),
+                    bias_variable([output_channel])))
+            elif activation_function == 'sigmoid':
+                out_layer = tf.nn.sigmoid(tf.add(tf.matmul(
+                    prev_layer, weight_variable([input_channel, output_channel])),
+                    bias_variable([output_channel])))
+            elif activation_function == 'tanh':
+                out_layer = tf.nn.tanh(tf.add(tf.matmul(
+                    prev_layer, weight_variable([input_channel, output_channel])),
+                    bias_variable([output_channel])))
+            else:
+                out_layer = tf.add(tf.matmul(
+                    prev_layer, weight_variable([input_channel, output_channel])),
+                    bias_variable([output_channel]))
 
-def load_dataset():
-    pass
 
-def model(learning_rate,
-            batch_size,
-            num_epochs,
-            display_step,
-            num_input,
-            num_classes):
+            prev_layer = out_layer
+            input_channel = output_channel
+
+    if activation_function == 'relu':
+        final_layer = tf.nn.relu(tf.add(tf.matmul(
+            prev_layer, weight_variable([input_channel, num_classes])),
+            bias_variable([num_classes])))
+    elif activation_function == 'sigmoid':
+        final_layer = tf.nn.sigmoid(tf.add(tf.matmul(
+            prev_layer, weight_variable([input_channel, num_classes])),
+            bias_variable([num_classes])))
+    elif activation_function == 'tanh':
+        final_layer = tf.nn.tanh(tf.add(tf.matmul(
+            prev_layer, weight_variable([input_channel, num_classes])),
+            bias_variable([num_classes])))
+    else:
+        final_layer = tf.add(tf.matmul(
+            prev_layer, weight_variable([input_channel, num_classes])),
+            bias_variable([num_classes]))
+
+    return final_layer
+
+def save_model(hparam, save_step, sess, saver):
+    save_path = (SAVEDIR + hparam)
+    if not os.path.exists(save_path):
+        os.makedirs(save_path)
+    saver.save(sess, (save_path + '/model.ckpt'), global_step=save_step)
+    print('Model saved to %s' % save_path)
+
+def mnist_model(learning_rate,
+                batch_size,
+                num_epochs,
+                display_step,
+                num_neurons,
+                num_layers,
+                num_input,
+                num_classes,
+                hparam,
+                save_step,
+                activation_function):
 
     # Reset the graph
     tf.reset_default_graph()
@@ -68,7 +142,7 @@ def model(learning_rate,
     y_ = tf.placeholder(tf.float32, shape=[None, num_classes])
 
     # Output of network
-    y = neural_network(x)
+    y = neural_network(x, num_input, num_layers, num_neurons, num_classes, activation_function)
 
     # Cost function
     with tf.name_scope('cross_entropy'):
@@ -96,28 +170,64 @@ def model(learning_rate,
 
     # Initialize all variables
     sess.run(tf.global_variables_initializer())
-    writer = tf.summary.FileWriter(SUMDIR)
+    writer = tf.summary.FileWriter(SUMDIR + hparam)
     writer.add_graph(sess.graph)
 
     # Train
     for step in range(num_epochs):
-        # get data from files
-        xray_images = load_dataset()
+        batch = mnist.train.next_batch(batch_size)
 
         # Display accuracy
         if step % display_step == 0:
             [train_accuracy, sum] = sess.run([accuracy, summary], feed_dict={x: mnist.test.images, y_: mnist.test.labels})
             print('training accuracy: %s, step: %d'%(train_accuracy, step))
             writer.add_summary(sum, step)
-        if step % 100 == 0:
-            save_model(step, sess, saver)
+        if step % save_step == 0:
+            save_model(hparam, step, sess, saver)
 
         # Training step
         sess.run(train_step, feed_dict={x: batch[0], y_: batch[1]})
 
+def make_hparam_string(batch_size, learning_rate, num_neurons, num_layers, activation_function):
+    return "batch=%d_lr_%.0E_neurons=%s_layers=%s_afunc=%s" % (batch_size, learning_rate, num_neurons, num_layers, activation_function)
 
-for i in range(1):
-    image_data = mpimg.imread(os.path.join(data_dir, 'train/NORMAL', train_normal_files[0]))
 
-plt.imshow(image_data)
-plt.show()
+def main():
+    # Parameters
+    num_epochs = 20000
+    display_step = 50
+    save_step = 2500
+
+    # Network Parameters
+    num_input = 40000  # data input (img shape: 200*200)
+    num_classes = 2  # classes (NORMAL, PNEUMONIA)
+
+    # Parameter search
+    batch_size = 5
+    neurons = 50
+    layers = 2
+    learning_rate = [0.1, 0.3, 0.5]
+    activation_function = 'relu'
+
+    hparam = make_hparam_string(batch_size, learning_rate, neurons, layers, activation_function)
+    print('Starting run for %s' % hparam)
+
+    mnist_model(learning_rate,
+                batch_size,
+                num_epochs,
+                display_step,
+                neurons,
+                layers,
+                num_input,
+                num_classes,
+                hparam,
+                save_step,
+                activation_function)
+
+    print('Done training!')
+    print('Run `tensorboard --logdir=%s` to see the results.' % SUMDIR)
+
+
+
+if __name__ == '__main__':
+    main()
