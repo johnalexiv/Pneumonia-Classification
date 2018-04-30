a9099b98e01a2eccaa2fd65c44cab318bd3dec93
John Alexander <jbalexander88@gmail.com>
2018-04-29 22:34:17 -0700

    fixed data load

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import tensorflow as tf
 import os
 import numpy as np
 from scipy.misc import imresize
-import pylab as pl
+import random
 from PIL import Image
 
 class DataSet():
@@ -11,6 +11,11 @@ class DataSet():
         self.train_dir = self.data_dir + '/train'
         self.val_dir = self.data_dir + '/val'
         self.test_dir = self.data_dir + '/test'
+        self.train_counter = 0
+        self.val_counter = 0
+        self.test_counter = 0
+        self.shuffle = True
+        self.skipped = 0
 
         self.train_normal_files = os.listdir(self.train_dir + '/NORMAL')
         self.train_pneumonia_files = os.listdir(self.train_dir + '/PNEUMONIA')
@@ -23,87 +28,119 @@ class DataSet():
 
         self.prepare_data()
 
+    def add_data(self, dir, files, images):
+        for file in files:
+            if not '.jpeg' in file:
+                continue
+            image = self.crop_resize_image(os.path.join(dir, file))
+
+            if image is None:
+                self.skipped += 1
+                continue
+
+            if 'NORMAL' in file:
+                images.append((image, 0))
+            else:
+                images.append((image, 1))
+
     def prepare_data(self):
         self.train_images = []
         self.val_images = []
         self.test_images = []
 
         # Load training data
-        for file in self.train_normal_files:
-            self.train_images.append((os.path.join(self.train_dir + '/NORMAL', file), 0))
-        for file in self.train_pneumonia_files:
-            self.train_images.append((os.path.join(self.train_dir + '/PNEUMONIA', file), 1))
+        self.add_data(self.train_dir + '/NORMAL', self.train_normal_files, self.train_images)
+        self.add_data(self.train_dir + '/PNEUMONIA', self.train_pneumonia_files, self.train_images)
+        self.add_data(self.val_dir + '/NORMAL', self.val_normal_files, self.val_images)
+        self.add_data(self.val_dir + '/PNEUMONIA', self.val_pneumonia_files, self.val_images)
+        self.add_data(self.test_dir + '/NORMAL', self.test_normal_files, self.test_images)
+        self.add_data(self.test_dir + '/PNEUMONIA', self.test_pneumonia_files, self.test_images)
 
-        # Load validation data
-        for file in self.val_normal_files:
-            self.val_images.append((os.path.join(self.val_dir + '/NORMAL', file), 0))
-        for file in self.val_pneumonia_files:
-            self.val_images.append((os.path.join(self.val_dir + '/PNEUMONIA', file), 1))
-
-        # Load test data
-        for file in self.test_normal_files:
-            self.test_images.append((os.path.join(self.test_dir + '/NORMAL', file), 0))
-        for file in self.test_pneumonia_files:
-            self.test_images.append((os.path.join(self.test_dir + '/PNEUMONIA', file), 1))
+        print('Skipped: %d'%self.skipped)
 
     def crop_resize_image(self, file_path):
-        img = Image.open(file_path)
+        try:
+            img = Image.open(file_path)
+            # print('File: %s, Shape: %s'% (file_path, img.size))
 
-        diff = abs(img.size[0] - img.size[1])
-        padding = int(diff / 2)
-        if img.size[0] > img.size[1]:
-            new_img = np.pad(img, [(padding, padding + 1), (0, 0)], mode='constant')
-        else:
-            new_img = np.pad(img, [(0, 0), (padding, padding + 1)], mode='constant')
+            diff = abs(img.size[0] - img.size[1])
+            odd = False
+            if diff % 2 == 1:
+                odd = True
+            padding = int(diff / 2)
+            if img.size[0] > img.size[1]:
+                if odd:
+                    new_img = np.pad(img, [(padding, padding + 1), (0, 0)], mode='constant')
+                else:
+                    new_img = np.pad(img, [(padding, padding), (0, 0)], mode='constant')
+            else:
+                if odd:
+                    new_img = np.pad(img, [(0, 0), (padding, padding + 1)], mode='constant')
+                else:
+                    new_img = np.pad(img, [(0, 0), (padding, padding)], mode='constant')
 
-        new_img = imresize(new_img, (200, 200), interp='bilinear')
+            new_img = imresize(new_img, (200, 200), interp='bilinear')
+        except:
+            print('Failed to load: File: %s' % file_path)
+            return None
 
         return np.asarray(new_img)
 
-    def next_image(self, type):
-        pass
-
     def next_batch(self, batch_size, type):
-        batch = []
-        for _ in range(batch_size):
-            pass
+        reset_counter = False
+        images = []
+        labels = []
 
+        if type == 'train':
+            batch_limit = self.train_counter + batch_size
 
-dataset = DataSet()
+            if self.shuffle:
+                random.shuffle(self.train_images)
+                self.shuffle = False
 
-import random
+            if batch_limit > len(self.train_images):
+                batch_limit = len(self.train_images)
+                reset_counter = True
+                self.shuffle = True
 
-a = dataset.test_images
-random.shuffle(a)
+            while self.train_counter < batch_limit:
+                label = np.zeros(2).astype(np.int32)
+                label[self.train_images[self.train_counter][1]] = 1
+                labels.append(label)
+                images.append(self.train_images[self.train_counter][0])
+                self.train_counter += 1
 
-image_files = [x[0] for x in a]
-label_values = [x[1] for x in a]
+            if reset_counter:
+                self.train_counter = 0
 
+        elif type == 'test':
+            batch_limit = self.test_counter + batch_size
 
-def crop_resize_image(file_path):
-    img = Image.open(file_path)
+            if batch_size == -1:
+                self.test_counter = 0
+                batch_limit = len(self.test_images)
 
-    diff = abs(img.size[0] - img.size[1])
-    padding = int(diff / 2)
-    if img.size[0] > img.size[1]:
-        new_img = np.pad(img, [(padding, padding + 1), (0, 0)], mode='constant')
-    else:
-        new_img = np.pad(img, [(0, 0), (padding, padding + 1)], mode='constant')
+            if self.shuffle:
+                random.shuffle(self.test_images)
+                self.shuffle = False
 
-    new_img = imresize(new_img, (200, 200), interp='bilinear')
+            if batch_limit > len(self.test_images):
+                batch_limit = len(self.test_images)
+                reset_counter = True
+                self.shuffle = True
 
-    return np.asarray(new_img)
+            while self.test_counter < batch_limit:
+                label = np.zeros(2).astype(np.int32)
+                label[self.test_images[self.test_counter][1]] = 1
+                labels.append(label)
+                images.append(self.test_images[self.test_counter][0])
+                self.test_counter += 1
 
-images = []
-labels = []
+            if reset_counter:
+                self.test_counter = 0
 
-for i, file in enumerate(image_files[:5]):
-    label = np.zeros(2).astype(np.int32)
-    label[label_values[i]] = 1
-    labels.append(label)
-    images.append(crop_resize_image(file))
+        return (np.reshape(np.array(images), (len(images), -1)), np.array(labels))
 
-batch = (np.reshape(np.array(images), (5, -1)), np.array(labels))
 
 LOGDIR = os.path.dirname(__file__)
 SAVEPATH = r'model_saves/'
@@ -117,7 +154,6 @@ if not os.path.exists(SAVEDIR):
 if not os.path.exists(SUMDIR):
     os.mkdir(SUMDIR)
 
-
 def weight_variable(shape):
     weight = tf.truncated_normal(shape, stddev=0.1)
     return tf.Variable(weight)
@@ -125,7 +161,6 @@ def weight_variable(shape):
 def bias_variable(shape):
     bias = tf.constant(0.1, shape=shape)
     return tf.Variable(bias)
-
 
 def save_model(save_step, sess, saver):
     save_path = (SAVEDIR)
@@ -135,15 +170,19 @@ def save_model(save_step, sess, saver):
     print('Model saved to %s' % save_path)
 
 def neural_network(input, input_size, num_classes):
-    x = tf.matmul(input, weight_variable([input_size, 32])) + bias_variable([32])
-    x = tf.nn.relu(x)
+    # x = tf.matmul(input, weight_variable([input_size, 256])) + bias_variable([256])
+    # x = tf.nn.relu(x)
+    #
+    # x = tf.matmul(x, weight_variable([256, 512])) + bias_variable([512])
+    # x = tf.nn.relu(x)
+    #
+    # x = tf.matmul(x, weight_variable([512, num_classes])) + bias_variable([num_classes])
+    # x = tf.nn.relu(x)
 
-    x = tf.matmul(x, weight_variable([32, 64])) + bias_variable([64])
-    x = tf.nn.relu(x)
+    # x = tf.matmul(input, weight_variable([input_size, num_classes])) + bias_variable([num_classes])
+    # x = tf.nn.relu(x)
 
-    x = tf.matmul(x, weight_variable([64, num_classes])) + bias_variable([num_classes])
-    x = tf.nn.relu(x)
-
+    hidden_layer = tf.add(tf.matmul(x))
     return x
 
 def save_model(hparam, save_step, sess, saver):
@@ -161,8 +200,8 @@ def mnist_model(learning_rate,
                 save_step):
 
     # Reset the graph
-    tf.reset_default_graph()
-    sess = tf.Session()
+    # tf.reset_default_graph()
+    # sess = tf.Session()
 
     # Setup placeholders, and reshape the data
     x = tf.placeholder(tf.float32, shape=[None, input_size])
@@ -187,33 +226,50 @@ def mnist_model(learning_rate,
         # Get prediction and calculate accuracy
         correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
         accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-        # tf.summary.scalar('accuracy', accuracy)
+        tf.summary.scalar('accuracy', accuracy)
 
     # Merge summaries
-    # summary = tf.summary.merge_all()
+    summary = tf.summary.merge_all()
 
     # Setup saver to save variables and graph
-    # saver = tf.train.Saver()
+    saver = tf.train.Saver()
 
     # Initialize all variables
+    # sess.run(tf.global_variables_initializer())
+    sess = tf.InteractiveSession()
     sess.run(tf.global_variables_initializer())
-    # writer = tf.summary.FileWriter(SUMDIR + hparam)
-    # writer.add_graph(sess.graph)
+    # tf.global_variables_initializer().run()
+    writer = tf.summary.FileWriter(SUMDIR + hparam)
+    writer.add_graph(sess.graph)
 
+    # for i in range(5000):
+    #     batch_xs, batch_ys = batch
+    #     sess.run(train_step, feed_dict={x: batch_xs, y_: batch_ys})
+    #
+    #     # Test trained model
+    #     if i % 10 == 0:
+    #         print(sess.run(accuracy, feed_dict={x: batch_xs,
+    #                                             y_: batch_ys}))
     # Train
+
+    dataset = DataSet()
+
     for step in range(num_epochs):
         # batch = mnist.train.next_batch(batch_size)
+        batch_xs, batch_ys = dataset.next_batch(50, 'train')
+        sess.run(train_step, feed_dict={x: batch_xs, y_: batch_ys})
 
         # Display accuracy
-        # if step % display_step == 0:
-        #     [train_accuracy, sum] = sess.run([accuracy, summary], feed_dict={x: mnist.test.images, y_: mnist.test.labels})
-        #     print('training accuracy: %s, step: %d'%(train_accuracy, step))
-        #     writer.add_summary(sum, step)
-        # if step % save_step == 0:
-            # save_model(hparam, step, sess, saver)
+        if step % 10 == 0:
+            batch_xs, batch_ys = dataset.next_batch(-1, 'test')
+            [train_accuracy, sum] = sess.run([accuracy, summary], feed_dict={x: batch_xs, y_: batch_ys})
+            print('training accuracy: %s, step: %d'%(train_accuracy, step))
+            writer.add_summary(sum, step)
+        if step % save_step == 0:
+            save_model(hparam, step, sess, saver)
 
         # Training step
-        sess.run(train_step, feed_dict={x: batch[0], y_: batch[1]})
+
 
 def make_hparam_string(batch_size, learning_rate, num_neurons, num_layers, activation_function):
     return "batch=%d_lr_%.0E_neurons=%s_layers=%s_afunc=%s" % (batch_size, learning_rate, num_neurons, num_layers, activation_function)
@@ -221,8 +277,7 @@ def make_hparam_string(batch_size, learning_rate, num_neurons, num_layers, activ
 
 def main():
     # Parameters
-    num_epochs = 1
-    display_step = 50
+    num_epochs = 10000
     save_step = 2500
 
     # Network Parameters
